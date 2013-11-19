33ba2d9b288d457a6d54d7c516d652fb6310bfb5
Gagaro <gagaro42@gmail.com>
2013-11-19 11:48:12 +0100

    fix: pep8

--- collective/memberdatatables/layer.py
+++ collective/memberdatatables/layer.py
@@ -1,4 +1,5 @@
 from zope.interface import Interface
 
+
 class ILayer(Interface):
     """Override Theme override of sharing view"""
--- collective/memberdatatables/sharing.py
+++ collective/memberdatatables/sharing.py
@@ -1,9 +1,7 @@
-from zope import interface
+from Acquisition import aq_inner
 from plone.app.workflow.browser import sharing
-from Acquisition import aq_inner, aq_parent, aq_base
-
 from zope.component import getMultiAdapter
-from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
 
 class SharingView(sharing.SharingView):
     """Override sharing view to use datatables"""
@@ -14,21 +12,19 @@ class SharingView(sharing.SharingView):
                                   get_principal_title,
                                   principal_type,
                                   id_key):
-
         context = aq_inner(self.context)
-        
-        existing_principals = set([p['id'] for p in self.existing_role_settings()
-                                if p['type'] == principal_type])
+        existing_principals = set(
+            [p['id'] for p in self.existing_role_settings()
+             if p['type'] == principal_type]
+            )
         empty_roles = dict([(r['id'], False) for r in self.roles()])
         info = []
-
         hunter = getMultiAdapter((context, self.request), name='pas_search')
 
         if principal_type == 'user':
             all = hunter.searchUsersByRequest(self.request, sort_by='fullname')
         else:
             all = hunter.searchGroupsByRequest(self.request)
-
         for principal_info in all:
             principal_id = principal_info[id_key]
             if principal_id not in existing_principals:
@@ -36,13 +32,12 @@ class SharingView(sharing.SharingView):
                 roles = empty_roles.copy()
                 if principal is None:
                     continue
-
                 for r in principal.getRoles():
                     if r in roles:
                         roles[r] = 'global'
-                info.append(dict(id    = principal_id,
-                                 title = get_principal_title(principal,
-                                                             principal_id),
-                                 type  = principal_type,
-                                 roles = roles))
+                info.append(dict(id=principal_id,
+                                 title=get_principal_title(principal,
+                                                           principal_id),
+                                 type=principal_type,
+                                 roles=roles))
         return info
